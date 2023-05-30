5e941e3250537371dd31c1bcfa84483f380e9a14
tyhopp <hopp.ty.c@gmail.com>
2023-05-30 20:38:40 +0800

    Make widget a server component so build passes

--- src/app/widgets/a/index.tsx
+++ src/app/widgets/a/index.tsx
@@ -1,14 +1,14 @@
-"use client"
-
 import { getLocale } from '../../utils/locales'
 
 export default async function WidgetA() {
   const locale = await getLocale('en')
+  const title = locale['widgets.a.title']
+  const description = locale['widgets.a.description']
 
   return (
     <div>
-      <h1>{locale['widgets.a.title']}</h1>
-      <p>{locale['widgets.a.description']}</p>
+      <h1>{title}</h1>
+      <p>{description}</p>
     </div>
   )
 }
